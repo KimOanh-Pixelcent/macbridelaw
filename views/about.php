f73f0daf684808d024a840810fd31a5f8cdb45fe
Ernie Turner <ernie.turner@ironcorelabs.com>
2017-10-20 20:18:52 -0600

    Update wording on about page

--- views/about.php
+++ views/about.php
@@ -32,16 +32,12 @@
                         </div>
                         <div class="column one-second">
                             <p class="big">
-                                Bill is the founder of MacBride Law, PLLC. Prior to founding MacBride Law, Mr. MacBride spend 21 years as a partner at the Helena, MT based firm Gough, Shanahan, Johnson
-                                and Waterman (GSJW). Prior to GSJW, Bill spent 6 years as an Attorney and Landman with Denver, CO based Gold Fields Mining Company, a wholly owned subsidiary of Consolidated Gold Fields of
-                                London, UK, representing Gold Fields in Canada, Taiwan and throughout the US. He brought to that
-                                company his 11 years of experience as a petroleum geologist, primarily with Pennzoil Company. He believes that his corporate and
-                                practical background is invaluable in providing effective representation to its clients.
+                                Bill assists clients to order to meet their business objectives, by providing timely responses in patent, trademarks, mining and other business matters.
                             </p>
                             <p class="big">
-                                Bill specializes in business organization, natural resources, real property, intellectual property (copyrights, patents and trademarks)
-                                and immigration laws. Bill graduated from Colorado School of Mines with a BS in Geological Engineering, from the University of Houston with
-                                an MBA in Finance and from the University of Denver with a JD.
+                                Bill is the founder of MacBride Law, PLLC. He spent 21 years as a partner at the Helena, MT based firm Gough, Shanahan, Johnson
+                                and Waterman, and 6 years as an with Denver based Gold Fields Mining Company, a subsidiary of Consolidated Gold Fields of
+                                London, representing Gold Fields in Canada, Taiwan and throughout the US.
                             </p>
                         </div>
                         <hr class="no_line hrmargin_b_1" />
@@ -57,17 +53,22 @@
                         <hr class="no_line hrmargin_b_40" />
                         <div class="column one-second">
                             <p class="big">
+                                Bill specializes in business organization, natural resources, real property, intellectual property (copyrights, patents and trademarks)
+                                and immigration laws. Bill graduated from Colorado School of Mines with a BS in Geological Engineering, from the University of Houston with
+                                an MBA in Finance and from the University of Denver with a JD.
+                            </p>
+                            <p class="big">
                                 Bill has authored or co-authored articles for various seminars and publications (in business, intellectual property, Chinese mineral
                                 resources, environmental and patent laws). He has taught environmental, natural resources and business law for the Community College
-                                of Aurora (Colorado) (1991-94), the University of Denver Graduate Studies Program (1993), the University of Montana-Helena, and Montana Tech, respectively. Between 1996-1998 he organized and presented programs on mineral economic
-                                evaluation and mineral interest tenure before the Chinese Ministry of Geology and Mineral Resources in Helena and Beijing.
+                                of Aurora (Colorado), the University of Denver Graduate Studies Program, the University of Montana-Helena, and Montana Tech. Between 1996-1998 he presented programs on mineral economics
+                                and mining tenure before the Chinese Ministry of Land in Helena and Beijing.
                             </p>
                         </div>
                         <div class="column one-second">
                             <p class="big">
                                 Bill is a member of the Colorado and Montana Bar Associations and a registered patent attorney with the U.S. Patent and Trademark Office. He is an active member of Rocky Mountain
                                 Mineral Law Foundation and Rotary International. He was a co-founder of the City of Helena International Affairs Council and is on the board of the Montana Center for International
-                                Visitors, the Grant-Kohrs Ranch Foundation, and the University of Helena-Montana Foundation Board, as president. Currently, he is also president of the World Montana Board.
+                                Visitors, the Grant-Kohrs Ranch Foundation, and the University of Helena-Montana Foundation Board, as president.
                                 <br/>
                                 <br/>
                                 Past clients include: Gold Fields Mining, Peabody Coal Company, Natural Solutions, Montana Hydraulics, Montana Emergent Technologies,
